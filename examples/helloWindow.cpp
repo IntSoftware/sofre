f2c9aeee668121e091f41f6f3d8511a0ae4e1ca0
Eugene Hong <awidesky@naver.com>
2026-01-09 19:05:17 +0900

    add stub main for helloWindow.cpp

--- examples/helloWindow.cpp
+++ examples/helloWindow.cpp
@@ -1,8 +1,22 @@
 
-#include <sofre/sofre.hpp>
-
 #include <iostream>
+
+#include <sgi/sgi.hpp>
 
 int main() {
     std::cout << "Hello, Window!" << std::endl;
+    
+    spear::grapics::global().initialize();
+
+    auto engine = spear::sgi::createGraphicEngine();
+    engine->initialize();
+
+    auto window = engine->createWindow("Hello", 800, 600);
+
+    while (...) {
+        engine->renderFrame();
+    }
+
+    engine->shutdown();
+    spear::sgi::graphic_global().shutdown();
 }
