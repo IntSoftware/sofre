023e42d110c6d3e824c2877c6955895a8614ad35
Eugene Hong <awidesky@naver.com>
2026-02-02 20:07:38 +0900

    fix : make current context only if not

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,5 +1,6 @@
 #include "core.hpp"
 #include "enums_func.hpp"
+#include "os_detect.hpp"
 #include "gl_debug.hpp"
 
 #include <sofre/renderer.hpp>
@@ -18,6 +19,7 @@ struct Renderer::Renderer_GL {
     ~Renderer_GL() {
         if (m_window)
             glfwDestroyWindow(m_window);
+        m_window = nullptr;
     }
 
     GLFWwindow* m_window = nullptr;
@@ -33,7 +35,7 @@ Renderer::Renderer(const Window& desc, const Renderer* master) : m_view(), m_pro
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    if(IsMacOS) glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     #ifdef SOFRE_DEBUG
@@ -119,10 +121,12 @@ void Renderer::removeObject(const std::shared_ptr<Object>& obj) {
 }
 
 void Renderer::render(const Scene& scene) {
-    if (!gl->m_window)
+    if (!m_creat_success || !gl->m_window)
         return;
-        
-    glfwMakeContextCurrent(gl->m_window);
+    
+   if (glfwGetCurrentContext() != gl ->m_window)
+        glfwMakeContextCurrent(gl->m_window);
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     m_program.use();
